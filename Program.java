5cfd82e4e6bf98a19a4b1852e91ea2d748428631
Arthur Henry Martins Brito <131808877+aTu44@users.noreply.github.com>
2025-07-01 16:52:58 -0300

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,6 +1,7 @@
 import modelos.Slot;
 
-public class Principal {
+public class Program
+{
     public static void main(String[] args) {
         String hud = """
         **********************************
